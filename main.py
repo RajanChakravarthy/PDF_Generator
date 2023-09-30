6b455164e3e387032080e95fb15eab031fb8afc9
rajan Mahadevan <rajanchakravarthy@gmail.com>
2023-09-30 21:45:57 +0200

    added footer

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from fpdf import FPDF
 import pandas as pd
 
+# A4 Size 210mm*298mm
 pdf = FPDF(orientation='P', unit='mm', format='A4')
+# text beyond the page dimensions will not be printed
+pdf.set_auto_page_break(auto=False,margin=0 )
+
 df = pd.read_csv('topics.csv')
 
 for index, rows in df.iterrows():
@@ -11,6 +15,14 @@ for index, rows in df.iterrows():
         pdf.set_text_color(100,100,100)
         pdf.cell(w=0, h=12, txt=rows['Topic'], align='L', ln=1)
         pdf.line(10,20,200,20)
+
+        # Line break to move the cursor to the end of page
+        pdf.ln(265)
+
+        # Creating footer note
+        pdf.set_font(family='Times', style='I', size=12)
+        pdf.set_text_color(180,180,180)
+        pdf.cell(w=0, h=12, txt=rows['Topic'], align='R', ln=1)
 
 
 pdf.output('output.pdf')
